e01243f4d02e97e281c4f821a52b659464d94a63
7320 <7320@hujing.co>
2025-08-15 14:36:28 +0800

    测试为什么pod不成功

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -59,17 +59,9 @@ flutter pub get
 
 #echo "📚 获取 Pod 依赖..."
 #pod install
-#
-### 构建 iOS Release（不签名）
-##flutter build ios --no-codesign
-##
-##echo "✅ Flutter SDK 下载与构建成功，交给 Xcode Cloud 继续 Archive & 签名"
-#
-##ls Flutter/Generated.xcconfig  # 若存在，说明本地生成正常
+
 echo "📚 Flutter 构建..."
 # 构建一次 iOS 项目（触发 Generated.xcconfig 生成）
 flutter build ios --config-only --no-codesign # 仅生成配置，不完整构建
 
-## 检查文件是否存在
-#echo "📚 检查文件是否存在"
-#ls Flutter/Generated.xcconfig  # 若存在，说明本地生成正常
+echo "✅ Flutter SDK 下载与构建成功，交给 Xcode Cloud 继续 Archive & 签名"
